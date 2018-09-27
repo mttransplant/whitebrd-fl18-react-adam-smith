6972c212b0987a92e242cfa77e986d27ddc9ce9f
Adam Smith <apsmith@ccs.neu.edu>
2018-09-26 22:02:30 -0400

    finish setup after following tutorial

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+
+ReactDom.render(
+    <h1>hello there<h1/>,
+    document.getElementById('root')
+)
