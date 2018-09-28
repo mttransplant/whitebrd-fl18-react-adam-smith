e5f355ba855fee757b864dc89eda1defa6db20d9
Adam Smith <apsmith@ccs.neu.edu>
2018-09-27 21:05:10 -0400

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 ReactDom.render(
-    <h1>hello there<h1/>,
+    <div>
+        <h1>hello there</h1>
+    </div>,
     document.getElementById('root')
 )
