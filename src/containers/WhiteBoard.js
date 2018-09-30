08a1c24de0e009f6c4c520151ad438d44d616c02
Adam Smith <apsmith@ccs.neu.edu>
2018-09-30 13:16:13 -0400

    move state data from index.js to WhiteBoard.js

--- src/containers/WhiteBoard.js
+++ src/containers/WhiteBoard.js
@@ -1,8 +1,14 @@
 import React, {Component} from "react"
+import CourseService from "../services/CourseService"
+import CourseTable from "./containers/CourseTable"
 
 export default class WhiteBoard extends Component {
     constructor(props) {
         super(props);
-
+        this.courseService = new CourseService();
+        this.state = {
+            courses: this.courseService.findAllCourses()
+        }
     }
+
 }
--- src/index.js
+++ src/index.js
@@ -2,27 +2,6 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/font-awesome/css/font-awesome.css"
-import CourseService from "./services/CourseService";
-
-import CourseTable from "./containers/CourseTable"
-
-let myCourseService = new CourseService();
-let myCourses = myCourseService.findAllCourses()
-let newCourse =     {
-    "id": "345",
-    "title": "CS5400",
-    "modules": []
-}
-let updatedCourse = {
-    "id": "345",
-    "title": "CS5400 - Special Topics",
-    "modules": [
-        {
-            "id": "987",
-            "title": "just a test"
-        }
-    ]
-}
 
 ReactDom.render(
     <div>
